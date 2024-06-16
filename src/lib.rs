6300fe893da0d552c00c0a48a11622af3f6aa52a
Diogo Sequeira <diogocnsequeira@gmail.com>
2024-06-16 13:06:31 +0100

    add if conditional

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod gumball {
     }
 
     impl GumballMachine {
-        pub fn instantiate_gumball_machine(price: Decimal) -> ComponentAddress {
+        pub fn instantiate_gumball_machine(price: Decimal) -> Global<GumballMachine> {
             let bucket_of_gumballs: Bucket = ResourceBuilder::new_fungible(OwnerRole::None)
                 .divisibility(DIVISIBILITY_NONE) // Individual gumballs, cannot be a % of a gumball.
                 .metadata(metadata!(
@@ -21,18 +21,42 @@ mod gumball {
                 ))
                 .mint_initial_supply(100)
                 .into(); // Into converts this Resource builder into a bucket;
-                         // this because mint initial supply may return a type that isn't directly a "Bucket"
-
+                         // Mint initial supply may return a type that isn't directly a "Bucket"
             Self {
                 gumballs: Vault::with_bucket(bucket_of_gumballs),
                 gumball_xrd_vault: Vault::new(XRD),
                 price: price,
             }
-
             .instantiate()
             .prepare_to_globalize(OwnerRole::None)
             .globalize()
-            
+        }
+
+        pub fn get_price(&self) -> Decimal {
+            self.price
+        }
+
+        pub fn buy_gumball(&mut self, mut payment: Bucket) -> (Bucket, Bucket) {
+            // Check if the payment contains at least the required price
+            if payment.amount() < self.price {
+                panic!(
+                    "Insufficient funds: Required {}, but received {}",
+                    self.price,
+                    payment.amount()
+                );
+            }
+
+            // take our price in XRD out of the payment
+            // if the caller has sent too few, or sent something other than XRD, they'll get a runtime error
+            let our_share = payment.take(self.price);
+            self.gumball_xrd_vault.put(our_share);
+
+            // we could have simplified the above into a single line, like so:
+            // self.collected_xrd.put(payment.take(self.price));
+
+            // return a tuple containing a gumball, plus whatever change is left on the input payment (if any)
+            // if we're out of gumballs to give, we'll see a runtime error when we try to grab one
+            (self.gumballs.take(1), payment)
         }
     }
 }
